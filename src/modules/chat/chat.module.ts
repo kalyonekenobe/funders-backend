5c42b1e9691704ffd9c6f4c5ef3b3345fb4ffda3
Oleksandr Igumnov <igumssukr@gmail.com>
2025-05-17 18:47:44 +0300

    Updated chats configuration

--- src/modules/chat/chat.module.ts
+++ src/modules/chat/chat.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ChatController } from 'src/modules/chat/chat.controller';
+import { ChatGateway } from 'src/modules/chat/chat.gateway';
 import { ChatService } from 'src/modules/chat/chat.service';
 import { ChatMessageModule } from 'src/modules/chat/submodules/chat-message/chat-message.module';
 import { ChatRoleModule } from 'src/modules/chat/submodules/chat-role/chat-role.module';
 import { ChatToUserModule } from 'src/modules/chat/submodules/chat-to-user/chat-to-user.module';
+import { ChatToUserService } from 'src/modules/chat/submodules/chat-to-user/chat-to-user.service';
 
 @Module({
   imports: [ChatRoleModule, ChatToUserModule, ChatMessageModule],
   controllers: [ChatController],
-  providers: [ChatService],
+  providers: [ChatService, ChatGateway, ChatToUserService],
 })
 export class ChatModule {}
